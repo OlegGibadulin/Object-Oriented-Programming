2c61ff068b0db1eb10f59c2c557ac70068179f2c
OlegGibadulin <O.Gibadulin@yandex.ru>
2019-04-05 01:47:08 +0300

    init new test project

---
+++ test/testOOP/testOOP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testOOP
+//
+//  Created by Mac-HOME on 05/04/2019.
+//  Copyright © 2019 Mac-HOME. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
