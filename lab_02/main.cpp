a10f97da304bf4d963e97db81de936c170bc970c
OlegGibadulin <O.Gibadulin@yandex.ru>
2019-04-05 01:59:54 +0300

    lab_02

---
+++ lab_02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab_02
+//
+//  Created by Mac-HOME on 05/04/2019.
+//  Copyright © 2019 Mac-HOME. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
